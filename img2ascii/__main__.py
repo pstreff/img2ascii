68bb2dedb515ab4f968585894e57ca1581bfd230
pstreff <streff@convidera.com>
2019-07-04 21:13:29 +0200

    added argument options

--- img2ascii/__main__.py
+++ img2ascii/__main__.py
@@ -1,4 +1,17 @@
 from img2ascii import app
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('Image to ASCII Image')
+    parser.add_argument('-W', '--width', help='Width of output image', type=int)
+    parser.add_argument('-H', '--height', help='Height of output image', type=int)
+    parser.add_argument('-I', '--input', help='Path to input image', default='crash_bandicoot.jpeg')
+    parser.add_argument('-Fs', '--font-size', help='Font size to use', type=int, default=10)
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    app.run()
+    arguments = parse_args()
+    app.run(arguments)
--- img2ascii/app.py
+++ img2ascii/app.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 
 def get_max_char_size(char_list, fnt):
@@ -24,49 +24,72 @@ def sum_pixels_under_mask(grayscaled_image, height_from, height_to, width_from, 
     return pixel_sum
 
 
-def grayscaleImage(image: Image):
+def grayscale_image(image: Image):
     return image.convert('L')
 
 
-def run():
-    base = Image.open('test.jpg')
+def calculate_cols_and_rows(size: dict, character_size: dict):
+    cols = size['width'] // character_size['width']
+    print(str(cols) + ' Columns')
 
-    grayscaled_image = grayscaleImage(base)
+    rows = size['height'] // character_size['height']
+    print(str(rows) + ' Rows')
 
-    fnt = ImageFont.truetype('Courier.dfont', 10)
+    return cols, rows
+
+
+def choose_output_dimensions(arguments, image: Image):
+    output_width = arguments.width if arguments.width else image.size[0]
+    output_height = arguments.height if arguments.height else image.size[1]
+
+    return output_width, output_height
+
+
+def run(arguments):
+    base = Image.open(arguments.input)
+
+    grayscaled_image = grayscale_image(base)
+
+    fnt = ImageFont.truetype('Courier.dfont', arguments.font_size)
 
     char_list = '@%#*+=-:. '
     # char_list = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
     max_char_width, max_char_height = get_max_char_size(char_list, fnt)
 
-    cols = grayscaled_image.size[0] // max_char_width
     print('Original Size: ' + str(grayscaled_image.size))
-    print(str(cols) + ' Columns | Cutoff: ' + str(grayscaled_image.size[0] - (cols * max_char_width)) + ' Pixel')
 
-    rows = grayscaled_image.size[1] // max_char_height
-    print(str(rows) + ' Rows | Cutoff: ' + str(grayscaled_image.size[1] - (rows * max_char_height)) + ' Pixel')
+    output_width, output_height = choose_output_dimensions(arguments, grayscaled_image)
+    print('Output Image Size: Width: ' + str(output_width) + ' Height: ' + str(output_height))
 
-    output_image = Image.new('L', grayscaled_image.size, 255)
+    cols, rows = calculate_cols_and_rows(
+        size={'width': output_width, 'height': output_height},
+        character_size={'width': max_char_width, 'height': max_char_height}
+    )
+
+    mask_width = grayscaled_image.size[0] // cols
+    mask_height = grayscaled_image.size[1] // rows
+
+    print('Mask size: ' + str(mask_width) + 'x' + str(mask_height))
+
+    output_image = Image.new('L', (output_width, output_height), 255)
 
     draw = ImageDraw.Draw(output_image)
     grayscaled_image.show()
     for j in range(0, rows):
         for i in range(0, cols):
-            width_from = i*max_char_width
-            width_to = i*max_char_width + max_char_width - 1
-            height_from = j*max_char_height
-            height_to = j*max_char_height + max_char_height - 1
+            width_from = i*mask_width
+            width_to = i*mask_width + mask_width - 1
+            height_from = j*mask_height
+            height_to = j*mask_height + mask_height - 1
 
             pixel_sum = sum_pixels_under_mask(grayscaled_image, height_from, height_to, width_from, width_to)
 
-            average = pixel_sum / (max_char_width * max_char_height)
+            average = pixel_sum / (mask_width * mask_height)
             average = round(average * len(char_list) / 255)
 
             fitting_char = char_list[min(average, len(char_list)-1)]
-            draw.text((width_from, height_from), fitting_char, fill=0, font=fnt)
+            draw.text((i * max_char_width, j * max_char_height), fitting_char, fill=0, font=fnt)
 
     output_image.show()
-
-
-
+    ImageOps.invert(output_image).show()
